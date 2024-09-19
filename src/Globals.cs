1d1adfad30a77eed9c09edbaace3912a8d80f4b8
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-09-19 22:22:57 +0200

    added sysconsole as global include

---
+++ src/Globals.cs
@@ -0,0 +1,6 @@
+﻿#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+global using sysconsole = System.Console;
+#pragma warning restore CS8981
+
+
+namespace Unknown6656.Console;
