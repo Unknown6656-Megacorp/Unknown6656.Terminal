336ba5623b0e744ea3eef443d5dd31526f29a691
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-09-19 22:24:18 +0200

    compiled current version

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+
+//////////////////////////////////////////////////////////////////////////
+// Auto-generated 2024-09-19 22:24:03.939                               //
+// ANY CHANGES TO THIS DOCUMENT WILL BE LOST UPON RE-GENERATION         //
+//////////////////////////////////////////////////////////////////////////
+
+using System.Reflection;
+using System;
+
+[assembly: AssemblyVersion("1.0.13.8807")]
+[assembly: AssemblyFileVersion("1.0.13.8807")]
+[assembly: AssemblyInformationalVersion("v.1.0.13.8807, commit: e363ba39073b2ddf0d8f06bdefd8af6746c6cfe3")]
+[assembly: AssemblyCompany("Unknown6656")]
+[assembly: AssemblyCopyright("Copyright © 2020 - 2024, Unknown6656")]
+[assembly: AssemblyProduct("Unknown6656.Console by Unknown6656")]
+[assembly: AssemblyTitle("Unknown6656.Console")]
+
+namespace Unknown6656.Console;
+
+/// <summary>
+/// A global module containing some meta-data.
+/// </summary>
+public static class __module__
+{
+    /// <summary>
+    /// The library's author. This value is equal to the author of the GitHub repository associated with <see cref="RepositoryURL"/>.
+    /// </summary>
+    public const string Author = "Unknown6656";
+    /// <summary>
+    /// Development year(s).
+    /// </summary>
+    public const string Year = "2020 - 2024";
+    /// <summary>
+    /// The library's copyright information.
+    /// </summary>
+    public const string Copyright = "Copyright © 2020 - 2024, Unknown6656";
+    /// <summary>
+    /// The library's current version.
+    /// </summary>
+    public static Version? LibraryVersion { get; } = Version.Parse("1.0.13.8807");
+    /// <summary>
+    /// The Git hash associated with the current build.
+    /// </summary>
+    public const string GitHash = "e363ba39073b2ddf0d8f06bdefd8af6746c6cfe3";
+    /// <summary>
+    /// The name of the GitHub repository associated with <see cref="RepositoryURL"/>.
+    /// </summary>
+    public const string RepositoryName = "Unknown6656.Console";
+    /// <summary>
+    /// The URL of this project's GitHub repository.
+    /// </summary>
+    public const string RepositoryURL = "https://github.com/Unknown6656-Megacorp/Unknown6656.Console";
+    /// <summary>
+    /// The date and time of the current build (2024-09-19 22:24:03.939).
+    /// </summary>
+    public static DateTime DateBuilt { get; } = DateTime.FromFileTimeUtc(0x01db0ad1dec40a70L);
+}
