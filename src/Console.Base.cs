74a1f7d1406e4672d3dd40dac8725755f404ac4c
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-09-20 22:10:01 +0200

    continued implementing sysconsole members

--- src/Console.Base.cs
+++ src/Console.Base.cs
@@ -1,10 +1,12 @@
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 using System.Text;
+using System.Linq;
 using System.IO;
 using System;
 
+using Unknown6656.Generics;
 using Unknown6656.Runtime;
 
 namespace Unknown6656.Console;
@@ -14,6 +16,19 @@ namespace Unknown6656.Console;
 
 public static unsafe partial class Console
 {
+    /// <inheritdoc cref="sysconsole.Error"/>
+    public static TextWriter Error => sysconsole.Error;
+
+    /// <inheritdoc cref="sysconsole.In"/>
+    [UnsupportedOSPlatform(OS.ANDR)]
+    [UnsupportedOSPlatform(OS.BROW)]
+    [UnsupportedOSPlatform(OS.IOS)]
+    [UnsupportedOSPlatform(OS.TVOS)]
+    public static TextReader In => sysconsole.In;
+
+    /// <inheritdoc cref="sysconsole.Out"/>
+    public static TextWriter Out => sysconsole.Out;
+
     /// <inheritdoc cref="sysconsole.IsInputRedirected"/>
     public static bool IsInputRedirected => sysconsole.IsInputRedirected;
 
@@ -43,7 +58,9 @@ public static unsafe partial class Console
         get
         {
             if (OS.IsWindows)
+#pragma warning disable CA1416 // Validate platform compatibility
                 return sysconsole.CapsLock;
+#pragma warning restore CA1416
             else
                 throw _unsupported_os; // TODO
         }
@@ -70,5 +87,67 @@ public static unsafe partial class Console
                 SetVT100Bit(25, value);
         }
     }
+
+    /// <inheritdoc cref="sysconsole.CursorLeft"/>
+    public static int CursorLeft
+    {
+        get
+        {
+            if (OS.IsUnix || OS.IsWindows)
+                return sysconsole.CursorLeft;
+            else if (GetVT100CursorPosition() is (int left, _, _))
+                return left - 1;
+            else
+                throw new InvalidOperationException("Failed to get cursor position.");
+        }
+        set
+        {
+            if (OS.IsUnix || OS.IsWindows)
+                sysconsole.CursorLeft = value;
+            else
+                sysconsole.Write($"\e[{value + 1}G");
+        }
+    }
+
+    /// <inheritdoc cref="sysconsole.CursorTop"/>
+    public static int CursorTop
+    {
+        get
+        {
+            if (OS.IsUnix || OS.IsWindows)
+                return sysconsole.CursorTop;
+            else if (GetVT100CursorPosition() is (_, int top, _))
+                return top - 1;
+            else
+                throw new InvalidOperationException("Failed to get cursor position.");
+        }
+        set
+        {
+            if (OS.IsUnix || OS.IsWindows)
+                sysconsole.CursorTop = value;
+            else
+                sysconsole.Write($"\e[{value + 1};{CursorLeft}H");
+        }
+    }
+
+
+
+
+
+
+    private static (int X, int Y, int Page)? GetVT100CursorPosition()
+    {
+        if (GetRawVT100Report("[?6n", 'R') is ['\e', '[', '?', .. string response])
+        {
+            int[] parts = response.Split(';').ToArray(int.Parse);
+
+            return (parts[0], parts[1], parts[2]);
+        }
+        else
+            return null;
+    }
+
+    //public static int CursorSize { get; set; }
+
 
 }
